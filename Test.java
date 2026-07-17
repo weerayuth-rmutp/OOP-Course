b25bbe8d74c28050c5f8d97bf2c32893b826ffff
Weerayuth@rmutp <167685039+weerayuth-rmutp@users.noreply.github.com>
2026-07-17 15:10:35 +0700

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("RMUTP");
-        System.out.println("CPE");
-        System.out.println("cpe1223");
-    }
-    
-}
